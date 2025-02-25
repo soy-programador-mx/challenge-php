400821d20b3cbad5073a783ba55607a917641bfa
Soy programador <soytecnologicomx@gmail.com>
2025-02-25 21:50:08 +0000

    Valor correcto para ID de la clase User

--- src/Entity/User/Domain/ValueObjects/UserId.php
+++ src/Entity/User/Domain/ValueObjects/UserId.php
@@ -28,7 +28,7 @@ final class UserId
         }
 
         // UUID v4 validation
-        if (preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/', $value) !== 1) {
+        if (preg_match('/^[0-9a-f.]{23}$/', $value) !== 1) {
             throw new InvalidArgumentException('Invalid user id');
         }
     }
