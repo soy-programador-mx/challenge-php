08457d1ecc81a0cfb8de6bca520dacca48a5aa50
Soy programador <soytecnologicomx@gmail.com>
2025-02-25 21:50:51 +0000

    Test, validación de entidad User

---
+++ tests/UserTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use Jorgeaguero\Docfav\Entity\User\Domain\User;
+use Jorgeaguero\Docfav\Entity\User\Domain\ValueObjects\UserId;
+use Jorgeaguero\Docfav\Entity\User\Domain\ValueObjects\UserName;
+use Jorgeaguero\Docfav\Entity\User\Domain\ValueObjects\UserEmail;
+use Jorgeaguero\Docfav\Entity\User\Domain\ValueObjects\UserPassword;
+use Jorgeaguero\Docfav\Entity\User\Domain\ValueObjects\UserCreatedAt;
+
+final class UserTest extends TestCase
+{
+    public function testUserEntityCanBeCreated(): void
+    {
+        $id = new UserId(uniqid('', true));
+        $name = new UserName('Jorge');
+        $email = new UserEmail('test@localhost.dev');
+        $password = new UserPassword('testinG1!');
+        $createdAt = new UserCreatedAt();
+
+        $user = new User($id, $name, $email, $password, $createdAt);
+
+        $this->assertInstanceOf(User::class, $user);
+    }
+}
